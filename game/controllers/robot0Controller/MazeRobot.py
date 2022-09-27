1d2ba5f65c23c7e0feed4beb3cd1b43a8f8b4419
yusuf emad <yusufy2004@gmail.com>
2022-09-27 17:38:04 +0200

    adjusted camera position, color sensor(not final)

--- game/controllers/robot0Controller/MazeRobot.py
+++ game/controllers/robot0Controller/MazeRobot.py
@@ -9,6 +9,10 @@ class MazeRobot:
         self.right_wheel = robot.getDevice("wheel2 motor")
         self.left_wheel.setPosition(float('inf'))
         self.right_wheel.setPosition(float('inf'))
+        self.left_camera = robot.getDevice("camera_l")
+        self.right_camera = robot.getDevice("camera_r")
+        self.left_camera.enable(TIME_STEP)
+        self.right_camera.enable(TIME_STEP)
 
     def set_left_vel(self, v):
         self.left_wheel.setVelocity(v)
@@ -19,5 +23,5 @@ class MazeRobot:
 
     def run(self):
         while self.robot.step(TIME_STEP) != -1:
-            self.set_right_vel(5)
-            self.set_left_vel(5)
+            self.set_right_vel(0)
+            self.set_left_vel(0)
