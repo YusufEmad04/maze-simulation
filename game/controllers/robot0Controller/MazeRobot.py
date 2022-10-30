0a98bf0b2bee380648ef4c23090e8191ad8fdb38
yusuf emad <yusufy2004@gmail.com>
2022-10-30 20:06:22 +0200

    turn 90, move 1 tile

--- game/controllers/robot0Controller/MazeRobot.py
+++ game/controllers/robot0Controller/MazeRobot.py
@@ -1,14 +1,15 @@
 TIME_STEP = 32
 
+
 class MazeRobot:
     def __init__(self, robot):
         self.robot = robot
 
-        self.left_wheel = robot.getDevice("wheel1 motor")
-        self.left_wheel.setPosition(float('inf'))
-
-        self.right_wheel = robot.getDevice("wheel2 motor")
+        self.right_wheel = robot.getDevice("wheel1 motor")
         self.right_wheel.setPosition(float('inf'))
+
+        self.left_wheel = robot.getDevice("wheel2 motor")
+        self.left_wheel.setPosition(float('inf'))
 
         self.left_camera = robot.getDevice("camera_l")
         self.left_camera.enable(TIME_STEP)
@@ -28,23 +29,22 @@ class MazeRobot:
         self.gyro = robot.getDevice("gyro")
         self.gyro.enable(TIME_STEP)
 
-
         self.robot_pos = [0, 0]
         self.color_sensor_values = [0, 0, 0]
+        self.gyro_values = [0, 0, 0]
 
-        self.image = None
+        self.left_image = None
+        self.right_image = None
 
-        self.color_case=""
+        self.color_case = ""
 
-       
-        self.left_encoder = self.left_wheel.getPositionSensor()    # Encoder initialization
+        self.left_encoder = self.left_wheel.getPositionSensor()  # Encoder initialization
         self.right_encoder = self.right_wheel.getPositionSensor()
         self.left_encoder.enable(TIME_STEP)
         self.right_encoder.enable(TIME_STEP)
 
-    
         self.emitter = robot.getDevice("emitter")
-        self.x_dimension=-54
-        self.z_dimension=-54
-        self.tiles_cnt = int((abs(self.x_dimension) + abs(self.z_dimension))/12)
-        self.map =[[-1 for k in range(self.tiles_cnt*4)] for j in range(self.tiles_cnt*4)]
+        self.x_dimension = -54
+        self.z_dimension = -54
+        self.tiles_cnt = int((abs(self.x_dimension) + abs(self.z_dimension)) / 12)
+        self.map = [[-1 for k in range(self.tiles_cnt * 4)] for j in range(self.tiles_cnt * 4)]
--- game/controllers/robot0Controller/functions.py
+++ game/controllers/robot0Controller/functions.py
@@ -1,17 +1,58 @@
+import numpy as np
+
 from MazeRobot import MazeRobot
 import cv2
+
+max_speed = 6.28
 
 
 def set_left_vel(robot: MazeRobot, v):
     robot.left_wheel.setVelocity(-v)
 
+
 def set_right_vel(robot: MazeRobot, v):
     robot.right_wheel.setVelocity(-v)
 
 
+def move_forward(robot: MazeRobot, v):
+    set_left_vel(robot, v)
+    set_right_vel(robot, v)
+
+
+def turn_right(robot: MazeRobot, v):
+    set_left_vel(robot, v)
+    set_right_vel(robot, -v)
+
+
+def turn_left(robot: MazeRobot, v):
+    set_left_vel(robot, -v)
+    set_right_vel(robot, v)
+
+
+def turn_90_time_step(robot: MazeRobot):
+    x = 18
+    while robot.robot.step(32) != -1 and x >= 0:
+        get_all_values(robot)
+        # print gyro values
+        print("gyro values: ", robot.gyro_values)
+        x -= 1
+        turn_left(robot, 3.4868)
+
+
+def move_one_tile(robot: MazeRobot):
+    x = 28
+    while robot.robot.step(32) != -1 and x >= 0:
+        get_all_values(robot)
+        # print gyro values
+        print("gyro values: ", robot.gyro_values)
+        x -= 1
+        move_forward(robot, 6.221)
+
+
 def get_gps(robot: MazeRobot):
-    robot.robot_pos[0] = robot.gps.getValues()[0]
-    robot.robot_pos[1] = robot.gps.getValues()[2]
+    robot.robot_pos[0] = robot.gps.getValues()[0] * 100
+    robot.robot_pos[1] = robot.gps.getValues()[2] * 100
+
 
 def get_color_sensor(robot: MazeRobot):
     robot.image = robot.color_sensor.getImage()
@@ -19,103 +60,114 @@ def get_color_sensor(robot: MazeRobot):
     robot.color_sensor_values[1] = robot.color_sensor.imageGetGreen(robot.image, 1, 0, 0)
     robot.color_sensor_values[2] = robot.color_sensor.imageGetBlue(robot.image, 1, 0, 0)
 
+
+def get_gyro_values(robot: MazeRobot):
+    robot.gyro_values[0] = robot.gyro.getValues()[0]
+    robot.gyro_values[1] = robot.gyro.getValues()[1]
+    robot.gyro_values[2] = robot.gyro.getValues()[2]
+
+
+def get_cameras_values(robot: MazeRobot):
+    robot.left_image = np.frombuffer(robot.left_camera.getImage(), np.uint8).reshape(
+        (robot.left_camera.getHeight(), robot.left_camera.getWidth(), 4))
+    robot.right_image = np.frombuffer(robot.right_camera.getImage(), np.uint8).reshape(
+        (robot.right_camera.getHeight(), robot.right_camera.getWidth(), 4))
+
+    color_sensor_image = robot.color_sensor.getImage()
+    robot.color_sensor_values[0] = robot.color_sensor.imageGetRed(color_sensor_image, 1, 0, 0)
+    robot.color_sensor_values[1] = robot.color_sensor.imageGetGreen(color_sensor_image, 1, 0, 0)
+    robot.color_sensor_values[2] = robot.color_sensor.imageGetBlue(color_sensor_image, 1, 0, 0)
+
+
 def get_all_values(robot: MazeRobot):
     get_gps(robot)
     get_color_sensor(robot)
+    get_cameras_values(robot)
+    get_gyro_values(robot)
 
 
-def map_gen(robot: MazeRobot,x,z):
-    x_axis2indx = int((abs(robot.x_dimension)+x)/12)
-    z_axis2indx = int((abs(robot.z_dimension)+z)/12)
-    if(x_axis2indx==0):
-        x_axis2indx=2
+def map_gen(robot: MazeRobot, x, z):
+    x_axis2indx = int((abs(robot.x_dimension) + x) / 12)
+    z_axis2indx = int((abs(robot.z_dimension) + z) / 12)
+    if (x_axis2indx == 0):
+        x_axis2indx = 2
     else:
-        x_axis2indx=(4*x_axis2indx)+2
-    if(z_axis2indx==0):
-        z_axis2indx=2
+        x_axis2indx = (4 * x_axis2indx) + 2
+    if (z_axis2indx == 0):
+        z_axis2indx = 2
     else:
-        z_axis2indx=(4*z_axis2indx)+2
-    
+        z_axis2indx = (4 * z_axis2indx) + 2
 
-    if robot.map[z_axis2indx][x_axis2indx]==-1:
-        if robot.color_case=="white":
-            robot.map[z_axis2indx][x_axis2indx]=0
-        if robot.color_case=="orange":
-            robot.map[z_axis2indx][x_axis2indx]=3
-    for i in robot.map:
-        print(i)
-    #print(robot.map)
-    print("""""")
-        
+    return x_axis2indx, z_axis2indx
+
+    # if robot.map[z_axis2indx][x_axis2indx]==-1:
+    #     if robot.color_case=="white":
+    #         robot.map[z_axis2indx][x_axis2indx]=0
+    #     if robot.color_case=="orange":
+    #         robot.map[z_axis2indx][x_axis2indx]=3
+    # for i in robot.map:
+    #     print(i)
+    # #print(robot.map)
+    # print("""""")
 
 
+# def move_one_tile(dic):
+#     #print(dic)
+#     dic= int( (dic-int(dic))*100%10 )
+#
+#     if dic >=4 and dic <=5:
+#         return False
+#     else:
+#         return False
+#
+# # Avoid holes and swamps by looking at the RBG colour of the camera
+def viewColour(robot: MazeRobot, r, g, b):
+    color_case = ""
 
-def move_one_tile(dic):
-    #print(dic)
-    dic= int( (dic-int(dic))*100%10 )
-    
-    if dic >=4 and dic <=5:
-        return False
-    else:
-        return False
-
-# Avoid holes and swamps by looking at the RBG colour of the camera
-def viewColour(robot:MazeRobot, r,g,b):
-    color_case=""
-    
-    if (r>=200) and (g>=200) and (b>=200):
+    if (r >= 200) and (g >= 200) and (b >= 200):
         print("White")
-        robot.color_case ="white"
-    elif (r>=230) and (g>=200) and (g<=240) and (b>110) and (b<=160):
+        robot.color_case = "white"
+    elif (r >= 230) and (g >= 200) and (g <= 240) and (b > 110) and (b <= 160):
         print("Orange")
-        robot.color_case="orange"
-    elif (r<70) and (g<70) and (b<70):
+        robot.color_case = "orange"
+    elif (r < 70) and (g < 70) and (b < 70):
         print("Black")
-        robot.color_case="black"
+        robot.color_case = "black"
 
-
-
-
- 
 
 def cam(img):
-
-    
     rgb = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
     rgb_copy = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
-    #blanc = np.zeros(img.shape[:2] , dtype='uint8')
-    #blanc.fill(255)
-    #blanc = cv2.cvtColor(blanc, cv2.COLOR_BGR2RGB)
+    # blanc = np.zeros(img.shape[:2] , dtype='uint8')
+    # blanc.fill(255)
+    # blanc = cv2.cvtColor(blanc, cv2.COLOR_BGR2RGB)
 
-    mask1 = cv2.inRange(rgb, (200, 200, 200), (255, 255,255)) #masking white
-    rgb[mask1>0]=(255,0,0)
-    cv2.imshow("rgb" , rgb)
+    mask1 = cv2.inRange(rgb, (200, 200, 200), (255, 255, 255))  # masking white
+    rgb[mask1 > 0] = (255, 0, 0)
+    cv2.imshow("rgb", rgb)
 
     difference = cv2.subtract(rgb, rgb_copy)
     b, g, r = cv2.split(difference)
     if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
         print("No Victim detected")
 
-    else: 
+    else:
         print("white")
-        #img2 = np.zeros(mask1.shape[:2] , dtype='uint8')
-        #img2 = cv2.bitwise_xor(blanc,rgb,mask=mask1)
-        #img2 = cv2.bitwise_xor(blanc,rgb)
-        #img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-        
-        #cv2.imshow("JJ" , img2)
-        
-        mask2 = cv2.inRange(rgb_copy, (-50, -50, -50), (80, 80,80))
-        rgb_copy[mask2>0]=(0,0,255)
+        # img2 = np.zeros(mask1.shape[:2] , dtype='uint8')
+        # img2 = cv2.bitwise_xor(blanc,rgb,mask=mask1)
+        # img2 = cv2.bitwise_xor(blanc,rgb)
+        # img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+
+        # cv2.imshow("JJ" , img2)
+
+        mask2 = cv2.inRange(rgb_copy, (-50, -50, -50), (80, 80, 80))
+        rgb_copy[mask2 > 0] = (0, 0, 255)
         difference = cv2.subtract(rgb, rgb_copy)
         b, g, r = cv2.split(difference)
         if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
             print("Not found")
-        else: 
-         cv2.imshow("img2" ,rgb_copy)
-         print("victim detected")
+        else:
+            cv2.imshow("img2", rgb_copy)
+            print("victim detected")
     cv2.waitKey(1)
-
-
-
